9a13cd779b817ca7f1b3534d34f1991048b6675e
Konstantin Borisov <smeagol74@gmail.com>
2014-12-21 22:44:32 +0600

    + OptionIf

---
+++ src/main/scala/com/hitsoft/scala/OptionIf.scala
@@ -0,0 +1,9 @@
+package com.hitsoft.scala
+
+/**
+ * Shortcut instead of `if (condition) Option(value) else None` you may write `OptionIf(condition, value)`
+ */
+object OptionIf {
+  def apply[T](value: => T, condition: Boolean): Option[T] = if (condition) Option(value) else None
+  def apply[T](condition: Boolean, value: => T): Option[T] = if (condition) Option(value) else None
+}
